7dc91f6279185d0a083e96c67ecdcc1876a57027
RedLavaRus <github@redlava.ru>
2020-05-04 01:19:02 +0300

    v.0.0.9

--- resources/start.wei.php
+++ resources/start.wei.php
@@ -1,6 +1,69 @@
 <?php
 
 $content = '
- Лендинг начать игру
+<h3 class="download_h3">Шаг 1: Скачать лаунчер</h3>
+ <div class="download_block">
+ 
+
+    <div class="download_block_small">
+        <div style="background:url(/img/download/windfon.png); background-size: 300px;" class="download_block_small_img" >
+        Windows
+        <div class="download_block_ico">
+                <img src="/img/download/wind.png" class="download_block_ico1img">
+            </div>
+        </div>
+        <div class="download_block_text">
+Скачать лаунчер<br>
+Скачать лаунчер(Зеркало)<br>
+<br>
+Скачать java 32-bit<br>
+Скачать java 64-bit<br>
+
+        </div>
+    </div>
+
+
+    <div class="download_block_small">
+        <div style="background:url(/img/download/linuxfon.png); background-size: 300px;" class="download_block_small_img" >
+        Linux   
+        <div class="download_block_ico">
+            <img src="/img/download/linux.png" class="download_block_ico1img">
+            
+            </div>
+            </div>
+            <div class="download_block_text">
+    Скачать лаунчер<br>
+    Скачать лаунчер(Зеркало)<br>
+    <br>
+    Скачать java 32-bit<br>
+    Скачать java 64-bit<br>
+    Скачать java 32-bit<br>
+    Скачать java 64-bit<br>
+    <br><br>
+            </div>
+        </div>
+    
+
+
+    <div class="download_block_small">
+        <div style="background:url(/img/download/macfon.png); background-size: 300px;" class="download_block_small_img" >
+        Mac OS 
+        <div class="download_block_ico">
+                <img src="/img/download/mac.png" class="download_block_ico1img">
+        
+            </div>
+            </div>
+            <div class="download_block_text">
+    Скачать лаунчер<br>
+    Скачать лаунчер(Зеркало)<br>
+    <br>
+    Скачать java 64-bit<br>
+    
+            </div>
+        </div>
+ </div>
+
+ <h3 class="download_h3">Шаг 2: Зарегестрируйтесь</h3>
+
     ';
 ?>
